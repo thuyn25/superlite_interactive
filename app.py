64fa1a588faf256ec16706831bbfdc82da78b05c
thuyn25 <thuyn27@lsu.edu>
2026-03-08 16:51:51 -0500

    iSpectra Ver 1.0

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from astropy import units as u
 from specutils import Spectrum1D
 import csv
 import plotly.express as px
-st.set_page_config(page_title="🚀 Supernova Spectroscopic Analyzer", layout="wide")
+st.set_page_config(page_title="SN Spec Analyzer", layout="wide")
 
 # --- 1. Data Loading --- 
 @st.cache_data
@@ -34,9 +34,15 @@ LINE_DB, MAJOR_SPECIES = load_line_db()
 colors = px.colors.qualitative.Alphabet + px.colors.qualitative.Dark24
 COLOR_MAP = {species: colors[i % len(colors)] for i, species in enumerate(MAJOR_SPECIES)}
 
+SPECTRA_COLORS = px.colors.qualitative.Set1
+
 # --- 2. SIDEBAR CONTROLS ---
 st.sidebar.title('Controls')
-uploaded_file = st.sidebar.file_uploader(r"Upload Spectrum ($\lambda$, Flux)", type=["txt", "csv", "dat"])
+uploaded_files = st.sidebar.file_uploader(r"Upload Spectrum ($\lambda$, Flux)", type=["txt", "csv", "dat"], accept_multiple_files=True)
+
+if len(uploaded_files) > 5:
+    st.sidebar.warning('Only the first 5 files will be processed.')
+    uploaded_files = uploaded_files[:5]
 
 # st.sidebar.markdown("### Global Shift")
 # Added a global redshift to de-redshift the uploaded data
@@ -72,48 +78,62 @@ for i, species in enumerate(MAJOR_SPECIES):
         st.markdown("---")
 
 def load_data(file, z):
-    data = np.loadtxt(file)
-    # Primary axis will be Rest Wavelength
-    wavelength_rest = data[:, 0] / (1 + z)
-    flux = data[:, 1]
-    # Return both rest and original observed wavelengths for axis syncing
-    return wavelength_rest, data[:, 0], flux
+    try:
+        data = np.loadtxt(file, usecols=(0, 1), comments='#', unpack=True)
+        # Primary axis will be Rest Wavelength
+        wavelength_rest = data[0] / (1 + z)
+        flux = data[1]
 
+        flux_norm = flux / np.median(flux)  # Normalize flux for better visualization
+        # Return both rest and original observed wavelengths for axis syncing
+        return wavelength_rest, data[0], flux_norm
+    except Exception as e:
+        st.error(f"Error loading {file.name}: {e}")
+        return None, None, None
 # --- 4. MAIN INTERFACE ---
 st.title("🚀 Supernova Spectroscopic Analyzer")
 
-if uploaded_file:
-    wav_rest, wav_obs, flux = load_data(uploaded_file, global_z)
-
-    y_min, y_max = flux.min(), flux.max()
-    y_padding = (y_max - y_min) * 0.15
-
+if uploaded_files:
     fig = go.Figure()
 
-    # Plot the main spectrum
-    fig.add_trace(go.Scatter(
-        x=wav_rest, 
-        y=flux, 
-        name="Spectrum", 
-        showlegend=False,
-        line=dict(color='white', width=1)
-    ))
-    fig.update_yaxes(showticklabels=False)  # Hide y-axis labels for cleaner look
+    all_wav_rest, all_wav_obs= [], []
+
+    for idx, file in enumerate(uploaded_files):
+        wav_rest, wav_obs, flux = load_data(file, global_z)
+
+        spec_color = SPECTRA_COLORS[idx % len(SPECTRA_COLORS)]
+        # Plot the main spectrum
+        fig.add_trace(go.Scatter(
+            x=wav_rest, 
+            y=flux, 
+            name=file.name, 
+            showlegend=True,
+            line=dict(color=spec_color, width=1.3)
+        ))
+        fig.update_yaxes(showticklabels=False)  # Hide y-axis labels for cleaner look
+
+        all_wav_rest.extend([wav_rest.min(), wav_rest.max()])
+        all_wav_obs.extend([wav_obs.min(), wav_obs.max()])
+        # all_flux.extend([flux.min(), flux.max()])
     
-    # 2. Ghost Trace (Forces Top Axis to appear)
-    fig.add_trace(go.Scatter(
-        x=wav_obs, 
-        y=flux, 
-        xaxis='x2', # THIS IS THE KEY: it links to your xaxis2 definition
-        name="Observed Frame",
-        marker=dict(opacity=0), # Makes it invisible
-        line=dict(width=0),     # No line
-        showlegend=False,       # Don't show in legend
-        hoverinfo='none'        # Don't show in hover
-    ))
+        # 2. Ghost Trace (Forces Top Axis to appear)
+        fig.add_trace(go.Scatter(
+            x=wav_obs, 
+            y=flux, 
+            xaxis='x2', # THIS IS THE KEY: it links to your xaxis2 definition
+            marker=dict(opacity=0), # Makes it invisible
+            line=dict(width=0),     # No line
+            showlegend=False,       # Don't show in legend
+            hoverinfo='none'        # Don't show in hover
+        ))
+
+    # y_min, y_max = min(all_flux), max(all_flux)
+    # y_padding = (y_max - y_min) * 0.2  # Add
+    global_rest_min, global_rest_max = min(all_wav_rest), max(all_wav_rest)
+    global_obs_min, global_obs_max = min(all_wav_obs), max(all_wav_obs)
 
     vc = 299792.458 # km/s
-    
+
     # Plot identified lines
     for species, params in line_params.items():
         spec_color = COLOR_MAP[species]
@@ -123,7 +143,7 @@ if uploaded_file:
         # If species_z == global_z, only v_exp affects the position on the rest axis
         z_factor = (1 + params['z']) / (1 + global_z)
         
-        for idx, rest_wav in enumerate(lines):
+        for l_idx, rest_wav in enumerate(lines):
             shifted_wav = rest_wav * z_factor * (1 - params['v_exp'] / vc)
             
             fig.add_vline(
@@ -135,26 +155,24 @@ if uploaded_file:
             )
             
             # Add annotation for the species name
-            if idx == 0:
+            if l_idx == 0:
                 fig.add_annotation(
                     x=shifted_wav, y=1+0.00, yref="paper",
                     text=species, font=dict(color=spec_color, size=18),
                     showarrow=True, textangle=-90, yanchor="top", 
                 )
 
-    top_min = wav_rest.min() * (1 + global_z)
-    top_max = wav_rest.max() * (1 + global_z)
-
     # --- DUAL AXIS CONFIGURATION ---
     fig.update_layout(
         template="plotly_dark",
-        height=700,
+        height=750,
         margin=dict(t=100, b=40, l=10, r=10),
+        legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1, font=dict(size=14)),
         
         xaxis=dict(
             title=dict(text="Rest Wavelength (Å)", font=dict(size=18), standoff=10),
             tickfont=dict(size=14), # Size of the numbers on the axis
-            range=[wav_rest.min(), wav_rest.max()],
+            range=[2000, 10000],
             automargin=True,
             # minor=dict(
             #     ticklen=4, 
@@ -171,7 +189,7 @@ if uploaded_file:
             tickfont=dict(size=14),
             overlaying='x',
             side='top',
-            range=[top_min, top_max],
+            # range=[global_obs_min, global_obs_max],
             showgrid=False,
             # Add Minor Ticks here too
             # minor=dict(
@@ -182,7 +200,7 @@ if uploaded_file:
             # ticks="inside"
         ),
         yaxis=dict(title=dict(text="Flux", font=dict(size=18)), tickfont=dict(size=14),
-                   range=[y_min, y_max + y_padding], automargin=True),
+                automargin=True),
         hovermode="x unified"
     )
 
